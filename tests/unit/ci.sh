bfd68731fc1d393b2052a873467ce66888743a26
tianshuo78520a <707759223@qq.com>
2024-08-29 08:39:04 +0000

    Fix

--- tests/unit/ci.sh
+++ tests/unit/ci.sh
@@ -1,12 +1,6 @@
 function main(){
   speech_ci_path=`pwd`
 
-  echo "Start cli"
-  cd ${speech_ci_path}/cli
-  source path.sh
-  bash test_cli.sh
-  echo "End cli"
-
   echo "Start asr"
   cd ${speech_ci_path}/asr
   bash deepspeech2_online_model_test.sh
@@ -26,6 +20,11 @@ function main(){
   cd ${speech_ci_path}/vector
   python test_augment.py
   echo "End Vector"
+
+  echo "Start cli"
+  cd ${speech_ci_path}/cli
+  bash test_cli.sh
+  echo "End cli"
 }
 
 main
